5091133d0a05d1fbca25caf7c55ced340af3bd66
prabaprakash <prabaprakash@outlook.com>
2019-12-30 16:27:14 +0530

    #tower breaks

---
+++ tower-breakers-1.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the towerBreakers function below.
+function towerBreakers(n, m) {
+  return (m == 1 || n % 2 == 0 ? 2 : 1);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const nm = readLine().split(' ');
+
+        const n = parseInt(nm[0], 10);
+
+        const m = parseInt(nm[1], 10);
+
+        let result = towerBreakers(n, m);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
