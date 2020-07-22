5cb3a89956db288ab145d5139460b73262bd52c2
prabakaran19931593858554573 <prabaprakash@outlook.com>
2020-07-22 14:25:43 +0530

    next permutation

---
+++ bigger-is-greater.js
@@ -0,0 +1,72 @@
+// next permutation problems, https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the biggerIsGreater function below.
+function biggerIsGreater(array) {
+    array = array.split('');
+    // Find non-increasing suffix
+    var i = array.length - 1;
+    while (i > 0 && array[i - 1] >= array[i])
+        i--;
+    if (i <= 0)
+        return "no answer";
+    
+    // Find successor to pivot
+    var j = array.length - 1;
+    while (array[j] <= array[i - 1])
+        j--;
+    var temp = array[i - 1];
+    array[i - 1] = array[j];
+    array[j] = temp;
+    
+    // Reverse suffix
+    j = array.length - 1;
+    while (i < j) {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+    }
+    return array.join('');
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const T = parseInt(readLine(), 10);
+
+    for (let TItr = 0; TItr < T; TItr++) {
+        const w = readLine();
+
+        let result = biggerIsGreater(w);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
